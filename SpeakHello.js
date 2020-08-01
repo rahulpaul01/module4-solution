b406430b8fd4851649d8c506b0036bd1f2ecb017
rahulpaul01 <67965583+rahulpaul01@users.noreply.github.com>
2020-08-01 20:18:21 +0530

    Update SpeakHello.js

--- SpeakHello.js
+++ SpeakHello.js
@@ -1,9 +1,9 @@
 (function (window){
   var speakWord = "Hello";
-  var hellospeaker = {
+  var helloSpeaker = {
   speak: function (name){
   console.log(speakWord + " " + name);
    }
   };
-window.hellospeaker = hellospeaker;
+window.helloSpeaker = helloSpeaker;
 })(window);
